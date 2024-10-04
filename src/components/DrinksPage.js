ca1e7fa28b8747052532198c6cd90679bf0d3634
ebisoy <etbisoy2021@plm.edu.ph>
2024-10-05 00:43:10 +0800

    small changes

--- src/components/DrinksPage.js
+++ src/components/DrinksPage.js
@@ -3,54 +3,54 @@ import { Link } from 'react-router-dom'; // Import Link for navigation
 import './DrinksPage.css'; 
 import Footer from './Footer'; // Import Footer component
 
-const drinksData = [
+const snacksData = [
     { category: 'Iced Coffee', drinks: [
-        { name: 'Cappuccino', price: 89.00 },
-        { name: 'Caramel Macchiato', price: 89.00 },
-        { name: 'Coffee Latte', price: 89.00 },
-        { name: 'Mocha Latte', price: 89.00 },
-        { name: 'Toffee Caramel', price: 89.00 },
-        { name: 'Double Dutch', price: 89.00 }
+        { name: 'Cappuccino', price: 89.00, src: '/image/drink1.png' },
+        { name: 'Caramel Macchiato', price: 89.00, src: '/image/drink1.png' },
+        { name: 'Coffee Latte', price: 89.00, src: '/image/drink1.png' },
+        { name: 'Mocha Latte', price: 89.00, src: '/image/drink1.png' },
+        { name: 'Toffee Caramel', price: 89.00, src: '/image/drink1.png' },
+        { name: 'Double Dutch', price: 89.00, src: '/image/drink1.png' }
     ]},
     { category: 'Hot Coffee', drinks: [
-        { name: 'Cappuccino', price: 89.00 },
-        { name: 'Caramel Macchiato', price: 89.00 },
-        { name: 'Coffee Latte', price: 89.00 },
-        { name: 'Mocha Latte', price: 89.00 },
-        { name: 'Caffe Americano', price: 89.00 },
-        { name: 'Double Dutch', price: 89.00 }
+        { name: 'Cappuccino', price: 89.00, src: '/image/drink1.png' },
+        { name: 'Caramel Macchiato', price: 89.00, src: '/image/drink1.png' },
+        { name: 'Coffee Latte', price: 89.00, src: '/image/drink1.png' },
+        { name: 'Mocha Latte', price: 89.00, src: '/image/drink1.png' },
+        { name: 'Caffe Americano', price: 89.00, src: '/image/drink1.png' },
+        { name: 'Double Dutch', price: 89.00, src: '/image/drink1.png' }
     ]},
     { category: 'Chocolate Frappe', drinks: [
-        { name: 'Dark Chocolate', price: 109.00 },
-        { name: 'Milk Chocolate', price: 109.00 },
-        { name: 'Chocolate Hazelnut', price: 109.00 },
-        { name: 'Chocolate Truffles', price: 109.00 },
-        { name: 'Chocolate Kisses', price: 109.00 },
-        { name: 'Mocha', price: 109.00 }
+        { name: 'Dark Chocolate', price: 109.00 , src: '/image/drink1.png'},
+        { name: 'Milk Chocolate', price: 109.00 , src: '/image/drink1.png'},
+        { name: 'Chocolate Hazelnut', price: 109.00 , src: '/image/drink1.png'},
+        { name: 'Chocolate Truffles', price: 109.00 , src: '/image/drink1.png'},
+        { name: 'Chocolate Kisses', price: 109.00 , src: '/image/drink1.png'},
+        { name: 'Mocha', price: 109.00 , src: '/image/drink1.png'}
     ]},
     { category: 'Frappuccino', drinks: [
-        { name: 'Caramel Frappuccino', price: 119.00 },
-        { name: 'Java Chip Frappuccino', price: 119.00 },
-        { name: 'Mocha Frappuccino', price: 119.00 },
-        { name: 'White Mocha Frappuccino', price: 119.00 },
-        { name: 'Double Chocolate Frappuccino', price: 119.00 },
-        { name: 'Strawberry Frappuccino', price: 119.00 }
+        { name: 'Caramel Frappuccino', price: 119.00 , src: '/image/drink1.png'},
+        { name: 'Java Chip Frappuccino', price: 119.00 , src: '/image/drink1.png' },
+        { name: 'Mocha Frappuccino', price: 119.00, src: '/image/drink1.png' },
+        { name: 'White Mocha Frappuccino', price: 119.00, src: '/image/drink1.png' },
+        { name: 'Double Chocolate Frappuccino', price: 119.00, src: '/image/drink1.png' },
+        { name: 'Strawberry Frappuccino', price: 119.00 , src: '/image/drink1.png'}
     ]},
     { category: 'Milk Tea', drinks: [
-        { name: 'Classic Milk Tea', price: 99.00 },
-        { name: 'Wintermelon Milk Tea', price: 99.00 },
-        { name: 'Taro Milk Tea', price: 99.00 },
-        { name: 'Matcha Milk Tea', price: 99.00 },
-        { name: 'Hokkaido Milk Tea', price: 99.00 },
-        { name: 'Okinawa Milk Tea', price: 99.00 }
+        { name: 'Classic Milk Tea', price: 99.00 , src: '/image/drink1.png'},
+        { name: 'Wintermelon Milk Tea', price: 99.00 , src: '/image/drink1.png'},
+        { name: 'Taro Milk Tea', price: 99.00 , src: '/image/drink1.png'},
+        { name: 'Matcha Milk Tea', price: 99.00 , src: '/image/drink1.png'},
+        { name: 'Hokkaido Milk Tea', price: 99.00 , src: '/image/drink1.png'},
+        { name: 'Okinawa Milk Tea', price: 99.00 , src: '/image/drink1.png'}
     ]},
     { category: 'Lemonades', drinks: [
-        { name: 'Classic Lemonade', price: 89.00 },
-        { name: 'Strawberry Lemonade', price: 89.00 },
-        { name: 'Blueberry Lemonade', price: 89.00 },
-        { name: 'Mango Lemonade', price: 89.00 },
-        { name: 'Peach Lemonade', price: 89.00 },
-        { name: 'Lime Lemonade', price: 89.00 }
+        { name: 'Classic Lemonade', price: 89.00, src: '/image/drink1.png' },
+        { name: 'Strawberry Lemonade', price: 89.00 , src: '/image/drink1.png'},
+        { name: 'Blueberry Lemonade', price: 89.00 , src: '/image/drink1.png'},
+        { name: 'Mango Lemonade', price: 89.00 , src: '/image/drink1.png'},
+        { name: 'Peach Lemonade', price: 89.00 , src: '/image/drink1.png'},
+        { name: 'Lime Lemonade', price: 89.00 , src: '/image/drink1.png'}
     ]}
 ];
 
@@ -64,13 +64,13 @@ const DrinksPage = () => {
 
             <div className="drinks-page">
                 <h1>Our Drinks</h1>
-                {drinksData.map((category, index) => (
+                {snacksData.map((category, index) => (
                     <div key={index} className="drink-category">
                         <h2>{category.category}</h2>
                         <div className="drink-items">
                             {category.drinks.map((drink, i) => (
                                 <div key={i} className="drink-item">
-                                    <img src="/image/drink1.png" alt={drink.name} className="drink-img"/>
+                                    <img src={drink.src} alt={drink.name} className="drink-img"/>
                                     <div className="drink-info">
                                         <div className="drink-name-price">
                                             <p className="drink-name">{drink.name}</p>
