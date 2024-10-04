6d1e2f5be0da5d1fb0a39aaa270d5d9e024b95d9
ebisoy <etbisoy2021@plm.edu.ph>
2024-10-04 22:05:16 +0800

    page_for_drink
    may page na for drinks

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';  // React Router components
 import Banner from './components/Banner';
 import Navbar from './components/Navbar';
-import Details from './components/Details';
+import Details from './components/Details';  // Where the clickable image is
 import Tagline from './components/Tagline';
 import Faqs from './components/Faqs';
 import Footer from './components/Footer';
+import DrinksPage from './components/DrinksPage';  // Import the DrinksPage
 import './App.css';
 
 function App() {
   return (
-    <div style={{ width: '100%' }}>
-      <div className="scrollable">
-        <Navbar />  
-        <Banner />
-        <Details />
-        <Tagline />
-        <Faqs/>
-        <Footer/>
+    <Router>
+      <div style={{ width: '100%' }}>
+        <Navbar />  {/* Navbar stays on top */}
+        <div className="scrollable">
+          <Routes>
+            {/* Default route - your homepage */}
+            <Route
+              path="/"
+              element={
+                <>
+                  <Banner />
+                  <Details />  {/* Where the image is clickable */}
+                  <Tagline />
+                  <Faqs />
+                  <Footer />
+                </>
+              }
+            />
+            {/* Route for the drinks page */}
+            <Route path="/drinks" element={<DrinksPage />} />
+          </Routes>
+        </div>
       </div>
-    </div>
+    </Router>
   );
 }
 
